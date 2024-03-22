c2fd07dd0f5cdb4e43a8175152bc0a96be1a001d
topdeveloper <topdeveloper0908@gmail.com>
2024-03-22 03:31:46 +0200

    update customer page

--- src/app/customer/page.jsx
+++ src/app/customer/page.jsx
@@ -4,18 +4,12 @@ import React, { Suspense } from "react";
 import Cookies from "js-cookie";
 import { useSearchParams } from "next/navigation";
 
-export function Customer() {
-  const searchParams = useSearchParams();
-  const token = searchParams.get("token");
+export default function Customer() {
+  // const searchParams = useSearchParams();
+  // const token = searchParams.get("token");
 
-  if (token) {
-    Cookies.set("token", token);
-  }
+  // if (token) {
+  //   Cookies.set("token", token);
+  // }
   return <Dashboard isCustomer />;
-}
-
-export default function SuspenseUser() {
-  return (
-    <Customer />
-  );
 }
